7f8e7320bb048d0b42e7c6871c44a814e979989a
ROM <42848897+ROMVoid95@users.noreply.github.com>
2020-11-10 15:10:02 -0600

    Fix SpawnDim Features Events firing when not suppose to fire
    fixed #18

--- src/main/java/net/romvoid95/gctweaks/gc/features/SpawnDimension.java
+++ src/main/java/net/romvoid95/gctweaks/gc/features/SpawnDimension.java
@@ -17,9 +17,9 @@ import net.romvoid95.gctweaks.base.Feature;
 
 public class SpawnDimension extends Feature {
 
-	private static boolean	enableSpawnDimension, firstJoinOnly, everyDeath, useCoord;
-	private static int		spawnDimId;
-	private static String   spawnPos;
+	private static boolean enableSpawnDimension, firstJoinOnly, everyDeath, useCoord;
+	private static int spawnDimId;
+	private static String spawnPos;
 
 	@Override
 	public String category() {
@@ -35,15 +35,17 @@ public class SpawnDimension extends Feature {
 	@Override
 	public void syncConfig(String category) {
 		enableSpawnDimension = set(category, "enableFeature", false);
-		useCoord = set(category, "useCoord", false);
+		useCoord = set(category, "useCoord", "If \"true\" custom spawn will use the coordinates set in \"spawnCoords\" below\n [default: false]", false);
+		
 		firstJoinOnly = set(category, "firstJoinOnly",
 				"If \"true\" players are only sent to the spawn dimension"
-						+ " on their first time joining.\nIf \"false\" players are sent every join\n [default: false]", true);
-		everyDeath = set(category, "everyRespawn", "If \"true\" players are sent to the spawn dimension"
-				+ "every death respawn.\n [default: false]", false);
-		spawnDimId = set(category, "dimensionId",
-				"Set the Dimension ID of the planet/moon you want players join on\n"
-						+ "see file under `config\\GalacticTweaks\\ValidDimensions.txt` for valid dimension ID's\n", 0);
+						+ " on their first time joining.\nIf \"false\" players are sent every join\n [default: false]",
+				true);
+		everyDeath = set(category, "everyRespawn",
+				"If \"true\" players are sent to the spawn dimension" + "every death respawn.\n [default: false]",
+				false);
+		spawnDimId = set(category, "dimensionId", "Set the Dimension ID of the planet/moon you want players join on\n"
+				+ "see file under `config\\GalacticTweaks\\ValidDimensions.txt` for valid dimension ID's\n", 0);
 		spawnPos = set(category, "spawnCoords", "Set the spawn position for players to spawn at", "128,64,128");
 
 	}
@@ -55,39 +57,54 @@ public class SpawnDimension extends Feature {
 
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public void onPlayerJoin(PlayerLoggedInEvent event) {
-		if (event.player instanceof EntityPlayer) {
-			if (enableSpawnDimension) {
-				final EntityPlayer player = event.player;
-				final NBTTagCompound entityData = player.getEntityData();
-				final NBTTagCompound persistedData = entityData.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
-				entityData.setTag(EntityPlayer.PERSISTED_NBT_TAG, persistedData);
-				final String key = Ref.MOD_ID + ":" + "FirstSpawn";
-				if (!persistedData.getBoolean(key)) {
-					teleport((EntityPlayerMP) event.player, spawnDimId);
-					persistedData.setBoolean(key, true);
-				}
-			} else if (firstJoinOnly) {
-				teleport((EntityPlayerMP) event.player, spawnDimId);
-			}
+		if (enableSpawnDimension) {
+			handleLoggingEvent(event);
 		}
 	}
 
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		if (enableSpawnDimension) {
+			handleRespawnEvent(event);
+		}
+	}
+
+	private void handleLoggingEvent(PlayerLoggedInEvent event) {
 		if (event.player instanceof EntityPlayer) {
-			if (everyDeath) {
-				teleport((EntityPlayerMP) event.player, spawnDimId);
+			final EntityPlayer player = event.player;
+			if (firstJoinOnly) {
+				final NBTTagCompound entityData = player.getEntityData();
+				final NBTTagCompound persistedData = entityData.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+				entityData.setTag(EntityPlayer.PERSISTED_NBT_TAG, persistedData);
+				final String key = Ref.MOD_ID + ":" + "FirstSpawn";
+				if (!persistedData.getBoolean(key)) {
+					teleport((EntityPlayerMP) player, spawnDimId);
+					persistedData.setBoolean(key, true);
+				}
+			} else if (!firstJoinOnly) {
+				teleport((EntityPlayerMP) player, spawnDimId);
+			}
+		}
+	}
+
+	private void handleRespawnEvent(PlayerRespawnEvent event) {
+		if (everyDeath) {
+			if (event.player instanceof EntityPlayer) {
+				final EntityPlayer player = event.player;
+				teleport((EntityPlayerMP) player, spawnDimId);
 			}
 		}
 	}
 
 	private void teleport(EntityPlayerMP player, int dimId) {
-		BlockPos pos;;
+		BlockPos pos;
+		;
 		MinecraftServer mcServer = player.getServer();
 		final WorldServer world = mcServer.getWorld(dimId);
 		String[] spawnString = spawnPos.split(",");
-		if(useCoord) {
-			pos = new BlockPos(Integer.valueOf(spawnString[0]), Integer.valueOf(spawnString[1]), Integer.valueOf(spawnString[2]));
+		if (useCoord) {
+			pos = new BlockPos(Integer.valueOf(spawnString[0]), Integer.valueOf(spawnString[1]),
+					Integer.valueOf(spawnString[2]));
 		} else {
 			int y = world.getChunkFromBlockCoords(world.getSpawnPoint()).getHeight(world.getSpawnPoint());
 			pos = new BlockPos(world.getSpawnPoint().getX(), y, world.getSpawnPoint().getZ());
